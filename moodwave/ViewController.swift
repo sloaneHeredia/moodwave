9f7b45ed638f7f3bf4a98e8c62e4471338e07bf8
Sloane Heredia <sloane.heredia@gmail.com>
2022-06-28 14:42:38 -0500

    Initial Commit

---
+++ moodwave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moodwave
+//
+//  Created by Sloane Heredia on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
