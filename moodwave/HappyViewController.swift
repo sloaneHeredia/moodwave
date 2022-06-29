29d0cdd4c76ff720491cb52df581945d4928562d
hannachann <hpchan22@gmail.com>
2022-06-29 13:59:28 -0500

    Happy layout

---
+++ moodwave/HappyViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HappyViewController.swift
+//  moodwave
+//
+//  Created by Hanna Chan on 6/29/22.
+//
+
+import UIKit
+
+class HappyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func happyPlaylist(_ sender: UIButton) {
+        if let url=URL(string: "https://open.spotify.com/playlist/4G7Wec7ASxi9dvAW1pN2Ca?si=c0b616bf92bf49c4") {
+            UIApplication.shared.open(url,options:[:], completionHandler: nil)
+        }
+    }
+    
+    
+//https://open.spotify.com/playlist/4G7Wec7ASxi9dvAW1pN2Ca?si=c0b616bf92bf49c4
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
