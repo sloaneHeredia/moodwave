01e39daf70c238b1ce47ce44fbf0cfbd06dfba62
Sloane Heredia <sloane.heredia@gmail.com>
2022-06-30 10:26:00 -0500

    added in happy resource #1 and view controller
    made journal angry prompt larger

---
+++ moodwave/happyResourceViewController.swift
@@ -0,0 +1,36 @@
+//
+//  happyResourceViewController.swift
+//  moodwave
+//
+//  Created by Sloane Heredia on 6/30/22.
+//
+
+import UIKit
+
+class happyResourceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+    @IBAction func happyChemButton(_ sender: UIButton) {
+        if let url=URL(string: "https://www.happyfeed.co/research/4-brain-chemicals-make-you-happy") {
+            UIApplication.shared.open(url,options:[:], completionHandler: nil)
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
