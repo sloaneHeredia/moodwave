9c4bf2451d8bb2eee762cb9bf07202cbbc9ec03f
Keira <1keira.kw@gmail.com>
2022-06-30 22:51:49 -0700

    added notification feature!

--- moodwave/ViewController.swift
+++ moodwave/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "moodwave"
+        content.body = "remember to do your daily check in!"
+        
+        let date = Date().addingTimeInterval(15)
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        center.add(request) { (error) in
+           
+        }
     }
 
 
 }
-
